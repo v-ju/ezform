a4f6c77ed36482449197acfeb38a5c2bc29f2592
Jaya <jaya.dev100x@gmail.com>
2025-10-04 22:24:10 +0530

    library setup

--- apps/FE/app/forms/page.tsx
+++ apps/FE/app/forms/page.tsx
@@ -1,3 +1,8 @@
+
+import OptionsButton from "@/compenents/OptionsButton";
+import Tiptap from "@/compenents/TipTap";
+import { Hexagon, LucidePanelTop, Palette } from "lucide-react";
+
 export default function Form(){
     return (
         <div className="h-dvh overscroll-contain" >
@@ -10,13 +15,24 @@ export default function Form(){
                 </div>
                 <div className="w-full">                    
                     <div className="mx-auto w-225">
-                        <div className="px-24 h-dvh ">
-                            <div className=" pb-10 bg-pink-100 text-5xl">
-                                <div>
-                                    <input placeholder="Form Title" className="border-0 outline-0 font-extrabold"></input>
-                                </div>
-                                
+                        <div className="px-24">   
+                                                    
+                            <div className="pb-10 group bg-pink-100 text-5xl">                               
+                                <div className="relative ">
+                                    <div className="hidden group-hover:block absolute -top-7 "> 
+                                        <div className="flex gap-2">
+                                            <OptionsButton buttonImage={<Hexagon size={20} color='gray' />} text="Add Logo"/>
+                                            <OptionsButton buttonImage={<LucidePanelTop size={18} color='gray'/>} text="Add Cover"/>
+                                            <OptionsButton buttonImage={<Palette size={18} color='gray'/>} text="Customize"/>
+                                        </div>
+                                    </div> 
+                                    <input placeholder="Form Title" className="border-0 outline-0 font-extrabold text-gray-600 w-full"/>                                     
+                                </div>                                
                             </div>
+                            
+                        </div>
+                        <div className="">
+                            <Tiptap/>                            
                         </div>
                     </div>
                 </div>
@@ -24,3 +40,4 @@ export default function Form(){
         </div>
     )
 }
+
--- apps/FE/compenents/blockElement.tsx
+++ apps/FE/compenents/blockElement.tsx
@@ -1,6 +1,6 @@
-export default function blockElement(){
+export default function BlockElement(){
     return (
-        <div>
+        <div className="py-2 empty:before:content-[attr(data-placeholder)] before:text-gray-400 before:left-2 before:top-2 before:pointer-events-none empty:before:block outline-none bg-blue-300" contentEditable="true" data-placeholder="Type '/' to insert block" data-empty>
             
         </div>
     )
